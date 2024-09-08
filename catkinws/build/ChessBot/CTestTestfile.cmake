52ad39bd9755b64eabfb9b0339f4ae3d03a87357
abdelrahman <abdelrahman.thabet@gmail.com>
2024-09-08 12:10:27 +0000

    added ros workspace

---
+++ catkinws/build/ChessBot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/speed/ChessBot/catkinws/src/ChessBot
+# Build directory: /home/speed/ChessBot/catkinws/build/ChessBot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkinws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/speed/ChessBot/catkinws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/speed/ChessBot/catkinws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/speed/ChessBot/catkinws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/speed/ChessBot/catkinws/build'
+export PYTHONPATH="/home/speed/ChessBot/catkinws/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/speed/ChessBot/catkinws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/speed/ChessBot/catkinws/src:$ROS_PACKAGE_PATH"
